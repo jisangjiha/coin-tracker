55d0ff4e5af4ded56fd7e47bd137809af5039b13
Jisang Hong <jisang1024@gmail.com>
2022-12-20 21:20:30 +0900

    change dollar to coin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [budget, setBudget] = useState(0);
+  const [selectedCoin, setSelectedCoin] = useState([]);
+  const selectCoin = (event) => {
+    setSelectedCoin(event.target.value);
+  };
+  const clac = (event) => {
+    setBudget(event.target.value);
+  };
+  const reset = () => {
+    setBudget(0);
+  };
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -11,21 +22,50 @@ function App() {
         setLoading(false);
       });
   }, []);
+
   return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
+    <>
+      <div>
+        <h1>The Coin {loading ? "" : `(${coins.length})`}</h1>
+        {loading ? (
+          <div>
+            <strong>Loading...</strong>
+          </div>
+        ) : (
+          <div>
+            <select onChange={selectCoin} required>
+              <option disabled defaultValue="">
+                Select the Coin!
+              </option>
+              {coins.map((coin) => (
+                <option key={coin.id} value={coin.quotes.USD.price}>
+                  {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+      </div>
+      <div>
+        <h3>How much do you have?</h3>
+        <input
+          id="budget"
+          type="number"
+          placeholder="input and enter"
+          value={budget}
+          onChange={clac}
+        />
+        USD
+        <h3>You can buy this coin!</h3>
+        <input
+          id="budget"
+          type="number"
+          value={Math.round((budget / selectedCoin) * 1000) / 1000}
+          disabled
+        />
+      </div>
+      <button onClick={reset}>Reset</button>
+    </>
   );
 }
 
